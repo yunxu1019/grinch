e10e96dc02e065e76fe8ee161853c6ee369831c4
不枝雀 <yunxu1019@qq.com>
2019-06-19 23:39:57 +0800

    支持添加,编辑,删除

--- apps/page/edit.js
+++ apps/page/edit.js
@@ -1,20 +1,41 @@
 var page = view();
 login();
-function main() {
+function main(item) {
     page.innerHTML = edit;
     render(page, {
         field,
         btn: button,
         go,
         input,
+        remove() {
+            this.remove.ing = true;
+            var data = this.data;
+            api("delete", `/grinch/${data._id}?rev=${data._rev}`).success(() => {
+                alert("删除成功！");
+                this.close();
+            }).error(() => {
+                this.remove.ing = false;
+                alert("删除失败！", "error");
+                render.refresh();
+            });
+        },
         save() {
-            api("put","/grinch/");
-            this.uploading = false;
+            var data = this.data;
+            if (!data._id) return;
+            this.save.ing = true;
+            api(data._rev ? "post" : "put", `/grinch/${data._id}`, data).success(() => {
+                alert("保存成功");
+                this.close();
+            }).error(() => {
+                alert("保存失败！", "error");
+                this.save.ing = false;
+                render.refresh();
+            });
         },
         close() {
-            history.back();
+            remove(page.mask);
         },
-        data: {}
+        data: Object.assign({ _id: user.name + ":" + +new Date, date: +new Date() }, item)
     });
     return page;
 }
--- apps/page/grinch.js
+++ apps/page/grinch.js
@@ -4,25 +4,32 @@ var scope = render(page, {
     items: [],
     btn: button,
     list: lattice,
-    popup,
+    load() {
+        cross("post", "http://efront.cc:5989/grinch/_find").send({
+            "selector": {
+            },
+            skip: 0,
+            limit: 21,
+            "sort": [{ 'date': "desc" }]
+        }).done(function (xhr) {
+            var items = JSON.parse(xhr.responseText).docs;
+            scope.items = items;
+            render.digest();
+            // return queue.call(items, function (item) {
+            //     return jsdom.JSDOM.fromURL(item.url, {
+            //         userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+            //     });
+            // });
+        });
+    },
+    popup() {
+        var elem = popup.apply(null, arguments);
+        once("remove")(elem, () => {
+            this.load();
+        })
+    },
 }).$scope;
 function main() {
-    cross("post", "http://efront.cc:5989/grinch/_find").send({
-        "selector": {
-        },
-        skip: 0,
-        limit: 21,
-        "sort": [{ 'date': "desc" }]
-    }).done(function (xhr) {
-        var items = JSON.parse(xhr.responseText).docs;
-        scope.items = items;
-        render.digest();
-        // return queue.call(items, function (item) {
-        //     return jsdom.JSDOM.fromURL(item.url, {
-        //         userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-        //     });
-        // });
-    });
-
+    scope.load();
     return page;
 }
