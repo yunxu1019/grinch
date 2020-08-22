e954e838e69d655681e8874ecb6262e2e2df0da1
不枝雀 <yunxu1019@qq.com>
2020-08-23 03:20:06 +0800

    资源列表

--- apps/page/grap.js
+++ apps/page/grap.js
@@ -1,11 +1,38 @@
 var page = div();
 page.innerHTML = grap;
 function main(params) {
-    render(page, {
+    renderWithDefaults(page, {
         button,
-        api: {
-            url: 'https://baidu.com',
-            method: 'get'
+        requests: [],
+        address: '/config/api.json',
+        editpage: false,
+        datas: [],
+        indexes: null,
+        load() {
+            console.log(this)
+            this.requests = data.from(this.address, a => {
+                this.datas = JSON.stringify(a, null, 4);
+                return this.parse(a);
+            });
+        },
+        parse(a) {
+            a = data.parseConfig(a);
+            return Object.keys(a).map(k => a[k]);
+
+        },
+        edit(a) {
+            this.editpage = true;
+        },
+        close() {
+            try {
+                var d = JSON.parse(this.datas);
+                var requests = this.parse(d);
+                this.requests.splice(0, this.requests.length);
+                this.requests.push.apply(this.requests, requests);
+            } catch (e) {
+                return alert.error('数据错误' + e);
+            }
+            this.editpage = false;
         },
         search() {
             var { url, method } = this.api;
