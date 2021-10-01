389c71b2bc2bcd17827ab6dee74fcbb2c30d4ae5
不枝雀 <yunxu1019@qq.com>
2021-10-02 00:57:50 +0800

    编译后自动提交

--- tools/build.bat
+++ tools/build.bat
@@ -1,6 +1,15 @@
 pushd
+setlocal
 cd %~dp0..
 call efront build
 for /d %%i in (..\yunxu1019.github.io\*) do rd /s /q %%i
 xcopy /s /y /q public ..\yunxu1019.github.io\
+cd ..\yunxu1019.github.io\
+git checkout --orphan clean
+git branch -D master
+git branch -m master
+git add .
+git commit -m 新版本
+git push --set-upstream origin master --force
+endlocal
 popd
