e20c3c08f9c937281ae80bd27094a984ab04a517
不枝雀 <yunxu1019@qq.com>
2022-01-18 04:25:02 +0800

    编译时不再推送到github.io

--- tools/build.bat
+++ tools/build.bat
@@ -3,14 +3,14 @@ pushd
 setlocal
 cd %~dp0..
 call efront build %*
-for /d %%i in (..\yunxu1019.github.io\*) do rd /s /q %%i
-xcopy /s /y /q public ..\yunxu1019.github.io\
-cd ..\yunxu1019.github.io\
-git checkout --orphan clean
-git branch -D master
-git branch -m master
-git add .
-git commit -m 新版本
-git push --set-upstream origin master --force
+@REM for /d %%i in (..\yunxu1019.github.io\*) do rd /s /q %%i
+@REM xcopy /s /y /q public ..\yunxu1019.github.io\
+@REM cd ..\yunxu1019.github.io\
+@REM git checkout --orphan clean
+@REM git branch -D master
+@REM git branch -m master
+@REM git add .
+@REM git commit -m 新版本
+@REM git push --set-upstream origin master --force
 endlocal
 popd
